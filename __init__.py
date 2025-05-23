4e6303cf574af7ac931c0a4af996a82d36409065
silveroxides <ishimarukaito@gmail.com>
2025-05-23 11:14:05 +0200

    added config file

--- __init__.py
+++ __init__.py
@@ -10,10 +10,12 @@ NODE_CLASS_MAPPINGS = {
 	"JJC_JoyCaption_Custom": nodes.JoyCaptionCustom,
 	"JJC_JoyCaption_DownloadAndLoad": nodes.JoyCaptionDownloadAndLoad,
 	"JJC_JoyCaption_Loader": nodes.JoyCaptionLoader,
+    "JJC_JoyCaption_ExtraOptions": nodes.JoyCaptionExtraOptions,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
 	"JJC_JoyCaption": "JoyCaption",
 	"JJC_JoyCaption_Custom": "JoyCaption (Custom)",
 	"JJC_JoyCaption_DownloadAndLoad": "JoyCaption Download And Load",
 	"JJC_JoyCaption_Loader": "JoyCaption Loader",
+    "JJC_JoyCaption_ExtraOptions": "JoyCaption Extra Options",
 }
